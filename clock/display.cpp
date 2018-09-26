011e7eb741a433fbf93b8c79cad65facd1370f76
pmarkiewicz <piotr.markiewicz@gmail.com>
2018-09-26 10:09:49 +0200

    merge

--- clock/display.cpp
+++ clock/display.cpp
@@ -10,12 +10,8 @@ const uint16_t PixelCount = 12;
 const uint8_t PixelPin = 14; // ignored
 
 const uint8_t pixMap[] = {6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5};
-<<<<<<< HEAD
-const uint8_t brightness[] = {25, 20, 20, 20, 20, 50, 60, 70, 80, 90, 200, 200, 200, 200, 200, 200, 180, 150, 140, 130, 100, 100, 80, 50, 0};
-=======
 const uint8_t brightness[] = {25, 20, 20, 20, 20, 50, 60, 70, 80, 90, 200, 200, 200, 200, 200, 200, 180, 150, 140, 130, 100, 100, 80, 40, 0};
 const uint8_t static_pts[] = {3, 6, 9, 0};
->>>>>>> 4906d9edcb78423c6ad69ee6b236c1a8f68951bf
 
 const uint8_t colorSaturation = 255; // saturation of color constants
 const RgbColor hh_color(colorSaturation, 0, 0);
